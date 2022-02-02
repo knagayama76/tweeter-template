30c66ba70ff7691063fac06dd5671a6269ed6ce9
knagayama76 <knagayama76>
2022-02-02 17:22:49 -0500

    render tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,67 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
+const createTweetElement = function (tweet) {
+  return `
+      <article class="tweet">
+      <header>
+        <div class="tweeter-name">
+          <img src=${tweet.user.avatars}>
+          <span>${tweet.user.name}</span>
+        </div>
+        <span class="name-handle">${tweet.user.handle}</span>
+      </header>
+      <div class="tweeter-content">
+        <p>
+         ${tweet.content.text}
+        </p>
+      </div>
+      <footer>
+        <time class="timeago">${timeago.format(
+          new Date(tweet.created_at)
+        )}</time>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `;
+};
+
+const renderTweets = function (tweets) {
+  $("#tweets-container").empty();
+  // loops through tweets
+  for (const tweet of tweets) {
+    // calls createTweetElement for each tweet
+    const tweetElement = createTweetElement(tweet);
+    $("#tweets-container").prepend(tweetElement);
+  }
+};
+
+$(() => {
+  renderTweets(data);
+});
