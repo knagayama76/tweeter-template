289d9fa388f3eebe5b4970cc5edfb3e5ac87d707
knagayama76 <knagayama76>
2022-02-03 19:26:37 -0500

    bug fixed on the counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,14 @@
 
 $(document).ready(function () {
   $("#tweet-text").on("input", function () {
-    const valLength = $(this).val().length;
-    const parent = $(this).siblings()[1];
+    const valLength = $("#tweet-text").val().length;
+    console.log(valLength);
+    // const parent = $(this).siblings()[1];
+    // console.log(parent);
     const leftover = 140 - valLength;
-    $($(parent).find(".counter")).text(leftover);
+    $(".counter").text(leftover);
     if (leftover < 0) {
-      $($(parent).find(".counter")).css({ color: "red" });
+      $(".counter").css({ color: "red" });
     }
   });
 });
